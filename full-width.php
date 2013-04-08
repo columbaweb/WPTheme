755dda186622e6363f4074f5f560ce620e47b8f1
Erika <erika.balode@gmail.com>
2013-04-08 02:24:47 +0100

    full width page template

---
+++ full-width.php
@@ -0,0 +1,22 @@
+<?php /* Template Name: Full Width */  ?>
+
+<?php get_header(); ?>
+
+<?php if (have_posts()): while (have_posts()): the_post(); ?>
+
+<section id="banner">
+   <div id="banner-title" class="grid_6">
+      <h1><?php the_title(); ?></h1>
+      <?php if(get_field('subtitle' ) != ""): ?>
+         <p><?php the_field('subtitle'); ?></p>
+      <?php endif; ?>
+   </div>   
+   <div class="breadcrumbs"><?php if(function_exists('bcn_display')){bcn_display();}?></div>
+</section>
+	
+<div id="content" <?php post_class('grid_12'); ?> >
+   <?php the_content(); ?>
+</div>
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
