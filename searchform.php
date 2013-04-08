5526c5fd997630d47dca652a71a9894d12800d38
Erika <erika.balode@gmail.com>
2013-04-08 02:26:16 +0100

    Search form

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form role="search" method="get" id="searchform" action="<?php bloginfo('url'); ?>">
+   <input type="text" name="s" id="s" value="Search..." onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/>
+   <input type="submit" id="searchsubmit" value="Search" />
+</form>
